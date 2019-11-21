6af4416832be7245480198579a6684caae972818
Guilherme Campanez <guilhermecampanez@gmail.com>
2019-11-21 14:13:39 -0300

    first commit for heroku

---
+++ build/precache-manifest.cd8381e22ebe573ba5df4da71fd8cdf7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a88d5f1337f0974bfe8691999ca08ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abb4f3869777c9270dc8",
+    "url": "/static/css/main.0abe2e85.chunk.css"
+  },
+  {
+    "revision": "e79ce069290d7486a306",
+    "url": "/static/js/2.65d3bbbd.chunk.js"
+  },
+  {
+    "revision": "abb4f3869777c9270dc8",
+    "url": "/static/js/main.e40b90c4.chunk.js"
+  },
+  {
+    "revision": "864730f065dbaae6b6b6",
+    "url": "/static/js/runtime-main.ac0c203a.js"
+  }
+]);
